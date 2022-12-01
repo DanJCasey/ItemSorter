39e684ecda38b0854b1e44dd5c7680ac431cc6e3
Daniel <daniel@Zipcoders-MacBook-Pro-10.local>
2022-12-01 15:12:41 -0500

    All tests passed, all code working. thank you to Andrew and Eryk

--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,32 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item> {
+    @Override
+public int compare(Item o1, Item o2) {
+        return o1.getName().compareTo(o2.getName());
 }
+}
+
+
+//    for(char c : Item)
+//    if(o1.getName().compareTo(o2.getName()))
+//        return 1;
+//    }
+//    if(o1.getId() < o2.getId()) {
+//        return -1;
+//    }
+//    if(o1.getId() == o2.getId()){
+//        return 0;
+//    }
+//    return 0;
+//
+//}
+//}
+//}
